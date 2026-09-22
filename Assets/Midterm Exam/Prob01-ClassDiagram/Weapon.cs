c4a685c66a893343dc83d6084f88b5f722fad85c
monxd <rattanasakhunchonpurin@gmail.com>
2026-09-22 16:44:29 +0700

    Midterm Examination

--- Assets/Midterm Exam/Prob01-ClassDiagram/Weapon.cs
+++ Assets/Midterm Exam/Prob01-ClassDiagram/Weapon.cs
@@ -2,8 +2,14 @@ using UnityEngine;
 
 namespace MidtermExam.Prob01
 {
-    public class Weapon
+    public class Weapon : Equipment
     {
         // TODO: Implement inheritance, fields and methods according to Class Diagram
+
+        public int extraDamage;
+        private float criticalChance;
+
+        public override void Equip(Hero hero) { }
+        public void Polish() { }
     }
 }
